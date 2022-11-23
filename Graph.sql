da2ecec8b2eeaf97cf32a78a506aa940ccbe9295
dmitrynovik <cryinstone@gmail.com>
2022-11-22 21:02:33 -0800

    apsp via

--- Graph.sql
+++ Graph.sql
@@ -22,16 +22,18 @@ INSERT INTO city VALUES
 ;
 
 INSERT INTO road VALUES
-	(0, 1, 102),
-	(0, 2, 59.8),
-	(0, 7, 208),
-	(2, 3, 85.4),
-	(3, 4, 62.1),
-	(3, 6, 252),
-	(4, 5, 56.9),
-	(6, 7, 312)
+	(0, 1, 102),   -- SYD -> Katoomba
+	(0, 2, 59.8),  -- SYD -> Windsor
+	(0, 7, 208),   -- SYD -> Singleton
+	(1, 3, 40.7),  -- Katoomba -> Lithgow
+	(2, 3, 85.4),  -- Windsor -> Lithgow
+	(3, 4, 62.1),  -- Bahurst -> Orange
+	(3, 6, 252),   -- Lithgow -> Dubbo
+	(4, 5, 56.9),  -- Bathurst -> Orange
+	(6, 7, 312)    -- Dubbo -> Singleton
 ;
 
+-- compute all pairs shortest path (APSP) 
 SELECT madlib.graph_apsp('city',             -- city table
                'id',                         -- city id column
                'road',                       -- road table
@@ -40,14 +42,17 @@ SELECT madlib.graph_apsp('city',             -- city table
                 );              
 				
 SELECT 
-		c1."name"  as from,
-		c2."name"  as to,
-		APSP.weight as distance
+		c1."name"  as from
+		,c2."name"  as to
+		,APSP.weight as distance
+		,via."name"  as via
+		--,*
 	FROM all_pairs_shorted_paths_out APSP
 	join city c1 on c1.id = src
 	join city c2 on c2.id = dest
+	join city via on via.id = parent
 	where src != dest
-	ORDER BY weight;
+	ORDER BY 1, 2;
 
 /*
 SELECT madlib.pagerank('city',     -- city table
